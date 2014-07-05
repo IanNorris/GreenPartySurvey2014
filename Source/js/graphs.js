b7deb03e6b3bfed434d48dad8cc5db9abc128239
Ian Norris <icstatic@gmail.com>
2014-07-05 01:27:19 +0100

    Refactored code significantly - graphs are now created from templates and JSON input data so there is less duplication. Split code across multiple files to make it easier to read. Minified spinner CSS.

---
+++ Source/js/graphs.js
@@ -0,0 +1,88 @@
+var graph_data = 
+{
+	graphs:
+	[
+		{
+			type: 'pie',
+			title: 'Would you want the Green Party to win a General Election?',
+			columnName: 'Want Green to win GE',
+			divName: 'wantGreenToWin',
+			description: "This chart illustrates whether the respondents would like the Green Party to win a General Election. Respondents that chose 'Yes, if changes are made' are those who are currently voting No, but indicated they may potentially change their voting intentions if a few policy alterations were made. To see what these policies are, click the pie chart slice and it will only show results from these responses.",
+			slices: 4,
+		},
+
+		{
+			type: 'pie',
+			title: 'Closest matching party',
+			columnName: 'Closest matching party',
+			divName: 'closestMatchingParty',
+			description: "TODO",
+			slices: 6,
+		},
+
+		{
+			type: 'pie',
+			title: 'How did you find this survey?',
+			columnName: 'Origin',
+			divName: 'referer',
+			description: "TODO",
+			slices: 5,
+		},
+
+		{
+			type: 'age',
+			title: 'How old are you?',
+			columnName: 'Age',
+			divName: 'age',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'What are the most important issues to you?',
+			columnName: 'Important policies',
+			divName: 'importantPolicies',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'What Green Party policies do you most approve of?',
+			columnName: 'Approval policies',
+			divName: 'approvalPolicies',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'What Green Party policies do you most disapprove of?',
+			columnName: 'Against policies',
+			divName: 'againstPolicies',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'What other key issues are most important to you?',
+			columnName: 'Key issues by name',
+			divName: 'keyIssues',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'Are there any specific Green Party policies or positions that you support?',
+			columnName: 'Unsolicited positive feedback',
+			divName: 'unsolicitedPositive',
+			description: "TODO",
+		},
+
+		{
+			type: 'multi',
+			title: 'Are there any specific Green Party policies that would prevent you from voting for them or that make you uneasy?',
+			columnName: 'Unsolicited negative feedback',
+			divName: 'unsolicitedNegative',
+			description: "TODO",
+		},
+	]
+}
---
+++ Source/js/survey.js
@@ -0,0 +1,290 @@
+var graphWidth = 800;
+var graphBarHeight = 1000;
+var graphHeight = 500;
+
+var graphs = {};
+
+String.prototype.format = function () {
+	var args = arguments;
+	return this.replace(/\{(\d+)\}/g, function (match, number) {
+		return typeof args[number] !== 'undefined' 
+			? args[number]
+			: match;
+	});
+};
+
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function reduceSum(attr) {
+	var functions =
+	{
+		add: function (p, v) {
+			if (v.hasOwnProperty(attr)) {
+				v[attr].forEach(
+					function (val, index) {
+						p[val] = (p[val] || 0) + 1;
+					}
+				);
+			}
+
+			return p;
+		},
+		remove: function (p, v) {
+			if (v.hasOwnProperty(attr)) {
+				v[attr].forEach(
+					function (val, index) {
+						p[val] = (p[val] || 0) - 1;
+					}
+				);
+			}
+
+			return p;
+		},
+		init: function () {
+			return {};
+		}
+	};
+	return functions;
+}
+
+function reduceSumGroup(dim, val) {
+	var rs = reduceSum(val);
+	var group = dim.groupAll().reduce(rs.add, rs.remove, rs.init).value();
+
+	group.all = function () {
+		var newObject = [];
+		for (var key in this) {
+			if (this.hasOwnProperty(key) && key != "all") {
+				newObject.push({
+					key: key,
+					value: this[key]
+				});
+			}
+		}
+		return newObject;
+	};
+
+	return group;
+}
+
+function createCategoryGraph(divName, data, columnName) {
+	var chart = dc.rowChart(divName);
+
+	var dimension = data.dimension(function (d) { return d[columnName]; });
+	var group = reduceSumGroup(dimension, columnName);
+
+	chart
+		.width(graphWidth)
+		.height(graphBarHeight)
+		.dimension(dimension)
+		.group(group)
+		.ordering(function (d) { return -d.value; })
+		.filterHandler(
+			function (dimension, filters) {
+				dimension.filter(null);
+
+				if (filters.length === 0) {
+					dimension.filter(null);
+				}
+				else {
+					dimension.filterFunction(function (d) {
+						if (d) {
+							for (var i = 0; i < d.length; i++) {
+								if (filters.indexOf(d[i]) >= 0) {
+									return true;
+								}
+							}
+						}
+						return false;
+					});
+				}
+
+				return filters;
+			}
+		)
+	;
+
+	graphs[columnName] = chart;
+}
+
+function createPieChart(divName, data, columnName, slices) {
+	var dimension = data.dimension(function (d) { return d[columnName]; });
+	var group = dimension.group().reduceSum(function (d) { return 1; });
+
+	group.all = function () {
+		var newObject = [];
+		for (var key in this) {
+			if (this.hasOwnProperty(key) && key != "all") {
+				newObject.push({
+					key: key,
+					value: this[key]
+				});
+			}
+		}
+		return newObject;
+	};
+
+	var chart = dc.pieChart(divName);
+	graphs[columnName] = chart;
+	chart
+			.width(graphWidth)
+			.height(graphHeight)
+			.slicesCap(slices)
+			.innerRadius(50)
+			.legend(dc.legend())
+			.dimension(dimension)
+			.group(group)
+	;
+}
+
+function createAgeChart(divName, data, columnName) {
+	var dimension = data.dimension(function (d) { return d[columnName]; });
+	var group = dimension.group().reduceSum(function (d) { return 1; });
+
+	var chart = dc.barChart(divName);
+	graphs[columnName] = chart;
+	chart
+			.width(graphWidth)
+			.height(graphHeight)
+			.x(d3.scale.linear().domain([14, 80]))
+			.xAxisLabel("Age")
+			.yAxisLabel("Count")
+			.dimension(dimension)
+			.group(group)
+	;
+}
+
+function createChartHtml( data, json )
+{
+	var graphTargetDiv = '#' + json.divName + 'Graph';
+
+	var graphTemplate = $("#graphTemplate").html();
+	var formattedTemplate = graphTemplate.format( json.title, json.divName + 'Graph', json.columnName, json.description );
+	$('#' + json.divName ).append( formattedTemplate );
+
+	switch( json.type )
+	{
+		case 'pie':
+			createPieChart( graphTargetDiv, data, json.columnName, json.slices );
+			break;
+
+		case 'multi':
+			createCategoryGraph( graphTargetDiv, data, json.columnName );
+			break;
+
+		case 'age':
+			createAgeChart( graphTargetDiv, data, json.columnName );
+			break;
+
+		default:
+			console.log( "Unknown chart type " + json.type );
+			break;
+	}
+}
+
+function processGraphs( data, json )
+{
+	for( var index = 0; index < json.graphs.length; index++ )
+	{
+		createChartHtml( data, json.graphs[ index ] );
+	}
+}
+
+var numberFormat = d3.format(".2f");
+
+var fieldsToCondenseAgainst =
+[
+	"Pro nuclear",
+	"Pro GM",
+	"Practicality of financial policy",
+	"Lack of experience",
+	"Unscientific",
+	"Animal testing",
+	"Holistic medicine",
+	"FPTP",
+	"Brighton council",
+	"Single issue party",
+	"Forced gender leadership",
+	"Open door immigration"
+];
+
+var fieldsToCondenseFor =
+[
+	"Favour renationalisation",
+	"Tuition fees",
+	"AGW",
+	"Citizens income",
+	"Other"
+];
+
+d3.csv("all.csv", function (csv) {
+	for (var index = 0; index < csv.length; index++) {
+		var row = csv[index];
+		row["Important policies"] = row["Important policies"].split(", ");
+		row["Against policies"] = row["Against policies"].split(", ");
+		row["Approval policies"] = row["Approval policies"].split(", ");
+		row["Key issues by name"] = row["Key issues by name"].split(", ");
+
+		row["Unsolicited negative feedback"] = [];
+		for (var fieldIndex = 0; fieldIndex < fieldsToCondenseAgainst.length; fieldIndex++) {
+			if (row.hasOwnProperty(fieldsToCondenseAgainst[fieldIndex]) && row[fieldsToCondenseAgainst[fieldIndex]] == "1") {
+				row["Unsolicited negative feedback"].push(fieldsToCondenseAgainst[fieldIndex]);
+			}
+		}
+
+		row["Unsolicited positive feedback"] = [];
+		for (var fieldIndex = 0; fieldIndex < fieldsToCondenseFor.length; fieldIndex++) {
+			if (row.hasOwnProperty(fieldsToCondenseFor[fieldIndex]) && row[fieldsToCondenseFor[fieldIndex]] == "1") {
+				row["Unsolicited positive feedback"].push(fieldsToCondenseFor[fieldIndex]);
+			}
+		}
+
+		for (var key in row) {
+			if (row.hasOwnProperty(key) && (row[key] == null || row[key] == "" || row[key] == "0" || (isNumber(row[key]) && +row[key] < "0"))) {
+				delete row[key];
+			}
+		}
+	}
+
+	var data = crossfilter(csv);
+
+	processGraphs( data, graph_data );
+
+	dc.renderAll();
+
+	$('.nav-pills li.menu a').on('click', function () {
+
+		var scrollAnchor = $(this).attr('data-scroll');
+		var scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 20;
+
+		$(".nav-pills li.active").removeClass("active");
+		$(this).parent('li').addClass('active');
+
+		$('body,html').animate(
+			{
+				scrollTop: scrollPoint
+			},
+			500
+		);
+
+		return false;
+
+	})
+
+
+	$(window).scroll(function () {
+		var windscroll = $(window).scrollTop();
+		$('.wrapper section').each(function (i) {
+			if ($(this).position().top <= windscroll - 20) {
+				$('.nav-pills li.menu').removeClass('active');
+				$('.nav-pills li.menu a[data-scroll=' + $(this).attr('data-anchor') + ']').parent('li').addClass('active');
+			}
+		});
+
+	}).scroll();
+
+	$('.loader').hide();
+	$('.mainContent').show();
+});
