b4492aa147183e22571db327455abfafe388c7b0
Ian Norris <icstatic@gmail.com>
2014-07-08 01:10:10 +0100

    Added graph descriptions

--- Source/js/graphs.js
+++ Source/js/graphs.js
@@ -16,7 +16,7 @@ var graph_data =
 			title: 'Closest matching party',
 			columnName: 'Closest matching party',
 			divName: 'closestMatchingParty',
-			description: "TODO",
+			description: "If the user completed the ISideWith survey and chose to share their result it will be displayed here. This gives a good approximation of leaning and is highly correlated against certain policy choices.",
 			slices: 6,
 		},
 
@@ -25,7 +25,7 @@ var graph_data =
 			title: 'How did you find this survey?',
 			columnName: 'Origin',
 			divName: 'referer',
-			description: "TODO",
+			description: "How did the responder find this survey? Reddit is by far the largest social network in the group as it has the largest immediate network for popular posts. I'm also glad to see a good turnout from the Green Party forms, meaning their voice of current members is well represented, although more people indicated they were Green Party members came from other sources.",
 			slices: 5,
 		},
 
@@ -34,7 +34,7 @@ var graph_data =
 			title: 'How old are you?',
 			columnName: 'Age',
 			divName: 'age',
-			description: "TODO",
+			description: "As expected there is a peak of those filling in the survey around the teen/young adult age. What was not expected is where the peak actually occurrs - 17. This is likely a bias due to the demographics of social media like Reddit and Twitter, but also a positive sign that the Green Party is attracting new voters - many of which will vote for the first time in 2015.",
 		},
 
 		{
@@ -42,7 +42,7 @@ var graph_data =
 			title: 'What are the most important policies to you?',
 			columnName: 'Important policies',
 			divName: 'importantPolicies',
-			description: "TODO",
+			description: "These are policies that the responder highlighted as of particular importance to them as a voter.",
 		},
 
 		{
@@ -50,7 +50,7 @@ var graph_data =
 			title: 'What Green Party policies do you most approve of?',
 			columnName: 'Approval policies',
 			divName: 'approvalPolicies',
-			description: "TODO",
+			description: "These are policies that the responder picked out as things the party is doing particularly well and are of relevance to them personally. These are policies that will contribute to a Yes vote for the Green Party.",
 		},
 
 		{
@@ -58,7 +58,7 @@ var graph_data =
 			title: 'What Green Party policies do you most disapprove of?',
 			columnName: 'Against policies',
 			divName: 'againstPolicies',
-			description: "TODO",
+			description: "These are policies that the responder highlighted as disagreeing or disapproving of. These are policies which will contribute to a No vote (a vote for another party).",
 		},
 
 		{
@@ -66,7 +66,7 @@ var graph_data =
 			title: 'What other key issues are most important to you?',
 			columnName: 'Key issues by name',
 			divName: 'keyIssues',
-			description: "TODO",
+			description: "These are the vote swinging single issues - the issues that will turn out voters.",
 		},
 
 		{
@@ -74,7 +74,7 @@ var graph_data =
 			title: 'Are there any specific Green Party policies or positions that you support?',
 			columnName: 'Unsolicited positive feedback',
 			divName: 'unsolicitedPositive',
-			description: "TODO",
+			description: "These are single issues that are well received by the responders and will contribute to a Yes vote for the Green Party.",
 		},
 
 		{
@@ -82,7 +82,7 @@ var graph_data =
 			title: 'Are there any specific Green Party policies that would prevent you from voting for them or that make you uneasy?',
 			columnName: 'Unsolicited negative feedback',
 			divName: 'unsolicitedNegative',
-			description: "TODO",
+			description: "This graph is arguably the most important of the lot - it shows what issues are preventing Green from receiving a larger percentage of the vote. Issues here are actively preventing votes and should be addressed immediately. This graph should be correlated against the 'Yes, if changes made' pie chart.",
 		},
 	]
 }
